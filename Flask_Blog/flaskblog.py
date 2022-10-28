4e4f32b0c7aecc618e0de57e4c080f82e8bc9e41
RoryM18 <40492068@live.napier.ac.uk>
2022-10-28 23:43:02 +0100

    Added home and about page along with some bootstrap, html and css need to change some things before continuing

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+        {
+            'author': 'Rory Mackintosh',
+            'title': 'Blog Post 1',
+            'content': 'First post content',
+            'date_posted': 'October 28, 2022'
+        },
+        {
+            'author': 'Amy Dickson',
+            'title': 'Blog Post 2',
+            'content': 'Second post content',
+            'date_posted': 'October 29, 2022'
+         }
+        ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
