ed9a6b5a06d30bd00bd914081e9129fdbb4a3694
RoryM18 <40492068@live.napier.ac.uk>
2022-10-31 15:21:27 +0000

    Finished adding forms and validation

--- Flask_Blog/forms.py
+++ Flask_Blog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -15,13 +15,12 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 
     email = StringField('Email', validators=[DataRequired(), Email()])
 
     password = PasswordField('Password', validators=[DataRequired()])
 
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    remember = BooleanField('Remember Me')
 
     submit = SubmitField('Sign Up!')
 
