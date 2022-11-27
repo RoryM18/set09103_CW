95649b4c743f8c8c1ba101b2f2cb06036970fcef
RoryM18 <40492068@live.napier.ac.uk>
2022-11-27 02:17:54 +0000

    Added custom error pages

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -190,3 +190,20 @@ def reset_token(token):
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
 
+
+#Create Custom error pages
+
+#Invalid URL
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+#Internal server error
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
